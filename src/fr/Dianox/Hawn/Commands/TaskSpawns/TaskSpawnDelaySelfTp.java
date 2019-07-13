33839b6b5097d7f876c7e2e5713c879aab7cc330
DianoxDragon <dianoxcontactpro@gmail.com>
2019-07-13 13:58:36 +0200

    Update TaskSpawnDelaySelfTp.java

--- src/fr/Dianox/Hawn/Commands/TaskSpawns/TaskSpawnDelaySelfTp.java
+++ src/fr/Dianox/Hawn/Commands/TaskSpawns/TaskSpawnDelaySelfTp.java
@@ -16,32 +16,50 @@ public class TaskSpawnDelaySelfTp extends BukkitRunnable {
 	private Player p;
 	
 	public TaskSpawnDelaySelfTp(Player p) {
+		/*
+		 * Main class to catch the variables
+		 */
         this.p = p;
 	}
 
 	@Override
 	public void run() {
 		if (p == null || !p.isOnline()) {
+			/*
+			 * If the player is null or not online
+			 * Cancel the task and stop the execution of the class
+			 */
             p = null;
             cancel();
             return;
         }
 		
+		/*
+		 * Then execute the code
+		 */
 		if (SpawnCommandConfig.getConfig().getBoolean("Commands.Spawn.CustomSpawn.Enable")) {
+			// If the value of the config is CHANGE ME by default
 			if (SpawnCommandConfig.getConfig().getString("Commands.Spawn.CustomSpawn.Spawn").contentEquals("CHANGE ME")) {
 				p.sendMessage("You have to change the spawn on Commands.CustomSpawn.Spawn on Events/OnJoin.yml");
 			} else {
+				// If the spawn is not set
 				if (!ConfigSpawn.getConfig().isSet("Coordinated."+SpawnCommandConfig.getConfig().getString("Commands.Spawn.CustomSpawn.Spawn"))) {
 					String Permission = "hawn.command.spawn."+SpawnCommandConfig.getConfig().getString("Commands.Spawn.CustomSpawn.Spawn");
 					MessageUtils.MessageNoPermission(p, Permission);
 					return;
 				}
+				
+				// If player don't have the permission
 				if (!p.hasPermission("hawn.command.spawn."+SpawnCommandConfig.getConfig().getString("Commands.Spawn.CustomSpawn.Spawn"))) {
 					String Permission = "hawn.command.spawn."+SpawnCommandConfig.getConfig().getString("Commands.Spawn.CustomSpawn.Spawn");
 					MessageUtils.MessageNoPermission(p, Permission);
 					return;
 				}
+				
+				// Teleport
 				SpawnUtils.teleportToSpawn(p, SpawnCommandConfig.getConfig().getString("Commands.Spawn.CustomSpawn.Spawn"));
+				
+				// Messages
 				if (ConfigMGeneral.getConfig().getBoolean("Spawn.Teleport.Enable")) {
 					for (String msg: ConfigMGeneral.getConfig().getStringList("Spawn.Teleport.Messages")) {
 						MessageUtils.ReplaceCharMessagePlayer(msg, p);
@@ -50,18 +68,26 @@ public class TaskSpawnDelaySelfTp extends BukkitRunnable {
 			}
 		} else {
 			if (OnJoinConfig.getConfig().getString("Spawn.DefaultSpawn").contentEquals("CHANGE ME")) {
+				// If the value of the config is CHANGE ME by default
 				p.sendMessage("You have to change the spawn on Spawn.DefaultSpawn on Events/OnJoin.yml");
 			} else {
+				// If the spawn is not set
 				if (!ConfigSpawn.getConfig().isSet("Coordinated."+OnJoinConfig.getConfig().getString("Spawn.DefaultSpawn"))) {
 					MessageUtils.MessageNoSpawn(p);
 					return;
 				}
+				
+				// If player don't have the permission
 				if (!p.hasPermission("hawn.command.spawn."+OnJoinConfig.getConfig().getString("Spawn.DefaultSpawn"))) {
 					String Permission = "hawn.command.spawn."+OnJoinConfig.getConfig().getString("Spawn.DefaultSpawn");
 					MessageUtils.MessageNoPermission(p, Permission);
 					return;
 				}
+				
+				// Teleport
 				SpawnUtils.teleportToSpawn(p, OnJoinConfig.getConfig().getString("Spawn.DefaultSpawn"));
+				
+				// Messages
 				if (ConfigMGeneral.getConfig().getBoolean("Spawn.Teleport.Enable")) {
 					for (String msg: ConfigMGeneral.getConfig().getStringList("Spawn.Teleport.Messages")) {
 						MessageUtils.ReplaceCharMessagePlayer(msg, p);
