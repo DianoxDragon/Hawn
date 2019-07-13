0984292b8d3115c052c3455b0246e5b03a265a54
DianoxDragon <dianoxcontactpro@gmail.com>
2019-07-13 14:02:29 +0200

    Update TaskSpawnDelaySelfTpSpawn.java

--- src/fr/Dianox/Hawn/Commands/TaskSpawns/TaskSpawnDelaySelfTpSpawn.java
+++ src/fr/Dianox/Hawn/Commands/TaskSpawns/TaskSpawnDelaySelfTpSpawn.java
@@ -16,6 +16,9 @@ public class TaskSpawnDelaySelfTpSpawn extends BukkitRunnable {
 	String tp;
 	
 	public TaskSpawnDelaySelfTpSpawn(Player p, String tp) {
+		/*
+		 * Main class to catch the variables
+		 */
         this.p = p;
         this.tp = tp;
 	}
@@ -23,43 +26,65 @@ public class TaskSpawnDelaySelfTpSpawn extends BukkitRunnable {
 	@Override
 	public void run() {
 		if (p == null || !p.isOnline()) {
+			/*
+			 * If the player is null or not online
+			 * Cancel the task and stop the execution of the class
+			 */
             p = null;
             cancel();
             return;
         }
 		
+		/*
+		 * Then execute the code
+		 */
+		// Check if hawn.command.spawn is enabled
 		if (SpawnCommandConfig.getConfig().getBoolean("Commands.Spawn.Use-Permission")) {
 			if (p.hasPermission("hawn.command.spawn")) {
+				// If the spawn is not set
 				if (!ConfigSpawn.getConfig().isSet("Coordinated."+tp)) {
 					MessageUtils.MessageNoSpawn(p);
 					return;
 				}
+				
+				// If player don't have the permission
 				if (!p.hasPermission("hawn.command.spawn."+tp)) {
 					String Permission = "hawn.command.spawn."+tp;
 					MessageUtils.MessageNoPermission(p, Permission);
 					return;
 				}
+				
+				// Teleport
 				SpawnUtils.teleportToSpawn(p, tp);
+				
+				// Messages
 				if (ConfigMGeneral.getConfig().getBoolean("Spawn.Teleport.Enable")) {
 					for (String msg: ConfigMGeneral.getConfig().getStringList("Spawn.Teleport.Messages")) {
 						MessageUtils.ReplaceCharMessagePlayer(msg, p);
 					}
 				}
 			} else {
-				String Permission = "hawn.command.spawn";
-				MessageUtils.MessageNoPermission(p, Permission);
+				// If player don't have the permission
+				MessageUtils.MessageNoPermission(p, "hawn.command.spawn");
 			}
 		} else {
+			// If the spawn is not set
 			if (!ConfigSpawn.getConfig().isSet("Coordinated."+tp)) {
 				MessageUtils.MessageNoSpawn(p);
 				return;
 			}
+			
+			// If player don't have the permission
 			if (!p.hasPermission("hawn.command.spawn."+tp)) {
 				String Permission = "hawn.command.spawn."+tp;
 				MessageUtils.MessageNoPermission(p, Permission);
 				return;
 			}
+			
+			// Teleport
 			SpawnUtils.teleportToSpawn(p, tp);
+			
+			// Messages
 			if (ConfigMGeneral.getConfig().getBoolean("Spawn.Teleport.Enable")) {
 				for (String msg: ConfigMGeneral.getConfig().getStringList("Spawn.Teleport.Messages")) {
 					MessageUtils.ReplaceCharMessagePlayer(msg, p);
