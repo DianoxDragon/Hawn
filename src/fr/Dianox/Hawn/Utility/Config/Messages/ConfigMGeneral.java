8a9503eb0e70a17e19fe46004c9c794f5891fadf
DianoxDragon <dianoxcontactpro@gmail.com>
2019-07-14 04:36:48 +0200

    Update ConfigMGeneral.java

--- src/fr/Dianox/Hawn/Utility/Config/Messages/ConfigMGeneral.java
+++ src/fr/Dianox/Hawn/Utility/Config/Messages/ConfigMGeneral.java
@@ -53,91 +53,92 @@ public class ConfigMGeneral {
                 file.createNewFile();
             } catch (IOException e) {}
 
-            Config.set("General.On-join.Join-Message.Enable", Boolean.valueOf(true));
-            Config.set("General.On-join.Join-Message.Just-Simply-Disable-All-Join-Messages", Boolean.valueOf(false));
-            Config.set("General.On-join.Join-Message.Disable-Default-Message", Boolean.valueOf(true));
-            Config.set("General.On-join.Join-Message.Silent-Staff-Join", Boolean.valueOf(true));
-            Config.set("General.On-join.Join-Message.Disable-For-New-Players", Boolean.valueOf(false));
-            Config.set("General.On-join.Join-Message.Broadcast-To-Console", Boolean.valueOf(true));
+            Config.set("General.Prefix", " &3Hawn &7|");
+            Config.set("General.On-join.Join-Message.Enable", true);
+            Config.set("General.On-join.Join-Message.Just-Simply-Disable-All-Join-Messages", false);
+            Config.set("General.On-join.Join-Message.Disable-Default-Message", true);
+            Config.set("General.On-join.Join-Message.Silent-Staff-Join", true);
+            Config.set("General.On-join.Join-Message.Disable-For-New-Players", false);
+            Config.set("General.On-join.Join-Message.Broadcast-To-Console", true);
             
-            Config.set("General.On-join.Join-Message.Per-Group.Options.Enable", Boolean.valueOf(true));
-            Config.set("General.On-join.Join-Message.Per-Group.Options.Disable-Any-Messages-On-Join", Boolean.valueOf(false));
+            Config.set("General.On-join.Join-Message.Per-Group.Options.Enable", true);
+            Config.set("General.On-join.Join-Message.Per-Group.Options.Disable-Any-Messages-On-Join", false);
             Config.set("General.On-join.Join-Message.Per-Group.Groups.Owner", java.util.Arrays.asList(new String[] {
                     "&cPLEASE WELCOME a owner"}));
             Config.set("General.On-join.Join-Message.Per-Group.Groups.Admin", java.util.Arrays.asList(new String[] {
                     "unlimited groups of course"}));
             
-            Config.set("General.On-join.Join-Message.Per-World.Options.Enable", Boolean.valueOf(false));
-            Config.set("General.On-join.Join-Message.Per-World.Options.Disable-Any-Other-Messages-On-Join", Boolean.valueOf(false));
-            Config.set("General.On-join.Join-Message.Per-World.Options.Only-Broadcast-Messages-In-The-World", Boolean.valueOf(false));
+            Config.set("General.On-join.Join-Message.Per-World.Options.Enable", false);
+            Config.set("General.On-join.Join-Message.Per-World.Options.Disable-Any-Other-Messages-On-Join", false);
+            Config.set("General.On-join.Join-Message.Per-World.Options.Only-Broadcast-Messages-In-The-World", false);
             Config.set("General.On-join.Join-Message.Per-World.Worlds.world", java.util.Arrays.asList(new String[] {
                     "&ctest1"}));
             Config.set("General.On-join.Join-Message.Per-World.Worlds.world_the_end", java.util.Arrays.asList(new String[] {
                     "test2"}));
             
             Config.set("General.On-join.Join-Message.Messages", java.util.Arrays.asList(new String[] {"&7[&a+&7] %player%"}));
-            Config.set("General.On-join.Join-Message.World.All_World", Boolean.valueOf(true));
+            Config.set("General.On-join.Join-Message.World.All_World", true);
             Config.set("General.On-join.Join-Message.World.Worlds", java.util.Arrays.asList(new String[] {
                     "world",
                     "world_nether"}));
             
-            Config.set("General.On-Quit.Quit-Message.Enable", Boolean.valueOf(true));
-            Config.set("General.On-Quit.Quit-Message.Just-Simply-Disable-All-Quit-Messages", Boolean.valueOf(false));
-            Config.set("General.On-Quit.Quit-Message.Disable-Default-Message", Boolean.valueOf(true));
-            Config.set("General.On-Quit.Quit-Message.Silent-Staff-Quit", Boolean.valueOf(true));
-            Config.set("General.On-Quit.Quit-Message.Broadcast-To-Console", Boolean.valueOf(true));
+            Config.set("General.On-Quit.Quit-Message.Enable", true);
+            Config.set("General.On-Quit.Quit-Message.Just-Simply-Disable-All-Quit-Messages", false);
+            Config.set("General.On-Quit.Quit-Message.Disable-Default-Message", true);
+            Config.set("General.On-Quit.Quit-Message.Silent-Staff-Quit", true);
+            Config.set("General.On-Quit.Quit-Message.Broadcast-To-Console", true);
             
-            Config.set("General.On-Quit.Quit-Message.Per-Group.Options.Enable", Boolean.valueOf(true));
-            Config.set("General.On-Quit.Quit-Message.Per-Group.Options.Disable-Any-Messages-On-Quit", Boolean.valueOf(true));
+            Config.set("General.On-Quit.Quit-Message.Per-Group.Options.Enable", true);
+            Config.set("General.On-Quit.Quit-Message.Per-Group.Options.Disable-Any-Messages-On-Quit", true);
             Config.set("General.On-Quit.Quit-Message.Per-Group.Groups.Owner", java.util.Arrays.asList(new String[] {
             		"&cPLEASE SAY GOODBYE a owner"}));
             Config.set("General.On-Quit.Quit-Message.Per-Group.Groups.Admin", java.util.Arrays.asList(new String[] {
             		"unlimited groups of course"}));
             
-            Config.set("General.On-Quit.Quit-Message.Per-World.Options.Enable", Boolean.valueOf(false));
-            Config.set("General.On-Quit.Quit-Message.Per-World.Options.Disable-Any-Other-Messages-On-Quit", Boolean.valueOf(false));
-            Config.set("General.On-Quit.Quit-Message.Per-World.Options.Only-Broadcast-Messages-In-The-World", Boolean.valueOf(false));
+            Config.set("General.On-Quit.Quit-Message.Per-World.Options.Enable", false);
+            Config.set("General.On-Quit.Quit-Message.Per-World.Options.Disable-Any-Other-Messages-On-Quit", false);
+            Config.set("General.On-Quit.Quit-Message.Per-World.Options.Only-Broadcast-Messages-In-The-World", false);
             Config.set("General.On-Quit.Quit-Message.Per-World.Worlds.world", java.util.Arrays.asList(new String[] {
                     "&ctest1"}));
             Config.set("General.On-Quit.Quit-Message.Per-World.Worlds.world_the_end", java.util.Arrays.asList(new String[] {
                     "test2"}));
             
             Config.set("General.On-Quit.Quit-Message.Messages", java.util.Arrays.asList(new String[] {"&7[&c-&7] %player%"}));
-            Config.set("General.On-Quit.Quit-Message.World.All_World", Boolean.valueOf(true));
+            Config.set("General.On-Quit.Quit-Message.World.All_World", true);
             Config.set("General.On-Quit.Quit-Message.World.Worlds", java.util.Arrays.asList(new String[] {
                     "world",
                     "world_nether"}));
             
-            Config.set("Spawn.On-join.Enable", Boolean.valueOf(true));
+            Config.set("Spawn.On-join.Enable", true);
             Config.set("Spawn.On-join.Messages", java.util.Arrays.asList(new String[] {
             		"&8&m<=====-------<&r &6Hawn &8&m>-------=====>",
                     "&cHello %player%",
                     "&cDon't forget to see our rules",
             		"&8&m<=====-------<&r &6Hawn &8&m>-------=====>"}));
-            Config.set("Spawn.On-join.World.All_World", Boolean.valueOf(true));
+            Config.set("Spawn.On-join.World.All_World", true);
             Config.set("Spawn.On-join.World.Worlds", java.util.Arrays.asList(new String[] {
                     "world",
                     "world_nether"}));
             
-            Config.set("Spawn.On-join.Per-World.Options.Enable", Boolean.valueOf(false));
-            Config.set("Spawn.On-join.Per-World.Options.Disable-All-The-Others-Motd", Boolean.valueOf(false));
+            Config.set("Spawn.On-join.Per-World.Options.Enable", false);
+            Config.set("Spawn.On-join.Per-World.Options.Disable-All-The-Others-Motd", false);
             Config.set("Spawn.On-join.Per-World.Worlds.world", java.util.Arrays.asList(new String[] {
 				"&ctest1 - motd"}));
             Config.set("Spawn.On-join.Per-World.Worlds.world_the_end", java.util.Arrays.asList(new String[] {
 				"test2 - motd"}));
             
-            Config.set("Spawn.On-join.First-Join.Broadcast.Enable", Boolean.valueOf(true));
-            Config.set("Spawn.On-join.First-Join.Broadcast.Broadcast-To-The-Console", Boolean.valueOf(true));
+            Config.set("Spawn.On-join.First-Join.Broadcast.Enable", true);
+            Config.set("Spawn.On-join.First-Join.Broadcast.Broadcast-To-The-Console", true);
             Config.set("Spawn.On-join.First-Join.Broadcast.Messages", java.util.Arrays.asList(new String[] {"&eWelcome %player% to the server"}));
-            Config.set("Spawn.On-join.First-Join.Motd.Enable", Boolean.valueOf(true));
-            Config.set("Spawn.On-join.First-Join.Motd.Both-Motd", Boolean.valueOf(false));
+            Config.set("Spawn.On-join.First-Join.Motd.Enable", true);
+            Config.set("Spawn.On-join.First-Join.Motd.Both-Motd", false);
             Config.set("Spawn.On-join.First-Join.Motd.Messages", java.util.Arrays.asList(new String[] {
             		"&8&m<=====-------<&r &6Hawn &8&m>-------=====>",
                     "&cWelcome %player%",
                     "&cDon't forget to see our rules",
             		"&8&m<=====-------<&r &6Hawn &8&m>-------=====>"}));
-            Config.set("Spawn.Teleport.Enable", Boolean.valueOf(true));
-            Config.set("Spawn.Teleport.Enable-For-On-Join", Boolean.valueOf(false));
+            Config.set("Spawn.Teleport.Enable", true);
+            Config.set("Spawn.Teleport.Enable-For-On-Join", false);
             Config.set("Spawn.Teleport.Messages", java.util.Arrays.asList(new String[] {"&7Teleport..."}));
             Config.set("Spawn.Teleport-By-Player.Messages", java.util.Arrays.asList(new String[] {"&7Teleported by someone..."}));
             Config.set("Spawn.Teleport-By-Player.Sender", java.util.Arrays.asList(new String[] {"&7Teleported %target%..."}));
