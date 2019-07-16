832fbec65b9f537eb59b3d9677e0f60ec0d208c3
DianoxDragon <dianoxcontactpro@gmail.com>
2019-07-16 11:31:26 +0200

    Create WarningCommandConfig.java

---
+++ src/fr/Dianox/Hawn/Utility/Config/Commands/WarningCommandConfig.java
@@ -0,0 +1,68 @@
+package fr.Dianox.Hawn.Utility.Config.Commands;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
+public class WarningCommandConfig {
+
+    private static Plugin pl;
+    private static File file;
+    private static YamlConfiguration Config;
+
+    public WarningCommandConfig() {}
+
+    public static void loadConfig(Plugin plugin) {
+        pl = plugin;
+
+        file = new File(pl.getDataFolder(), "Commands/Warning.yml");
+        Config = YamlConfiguration.loadConfiguration(file);
+
+        if (!pl.getDataFolder().exists()) {
+            pl.getDataFolder().mkdir();
+        }
+
+        create();
+    }
+
+    public static File getFile() {
+        return file;
+    }
+
+    public static YamlConfiguration getConfig() {
+        return Config;
+    }
+
+    public static void reloadConfig() {
+        loadConfig(pl);
+    }
+
+    public static void saveConfigFile() {
+        try {
+            Config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void create() {
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {}
+            
+            Config.set("Warning.Enable", true);
+            Config.set("Warning.Disable-Message", true);
+            Config.set("Warning.Sounds.Enabled", true);
+            Config.set("Warning.Sounds.Sound", "NOTE_PIANO");
+            Config.set("Warning.Sounds.Volume", 10);
+            Config.set("Warning.Sounds.Pitch", 1);
+            Config.set("DISABLE_THE_COMMAND_COMPLETELY", false);
+            
+            saveConfigFile();
+
+        }
+    }
+}
