e026387e9dad11daadc1b541b61871410b8dfdaa
DianoxDragon <dianoxcontactpro@gmail.com>
2019-07-28 04:00:28 +0200

    Update TaskSavePlayerServer.java

--- src/fr/Dianox/Hawn/Utility/Tasks/TaskSavePlayerServer.java
+++ src/fr/Dianox/Hawn/Utility/Tasks/TaskSavePlayerServer.java
@@ -4,9 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import fr.Dianox.Hawn.Utility.MessageUtils;
-import fr.Dianox.Hawn.Utility.Config.Messages.Adminstration.AdminPanelConfig;
-
 public class TaskSavePlayerServer extends BukkitRunnable {
 
 	private Player p;
@@ -19,10 +16,7 @@ public class TaskSavePlayerServer extends BukkitRunnable {
 	public void run() {
 		Bukkit.getServer().savePlayers();
 		
-		for (String msg : AdminPanelConfig.getConfig().getStringList("Special.Item.Save-Players.Messages")) {
-			MessageUtils.ReplaceCharMessagePlayer(msg, p);
-		}
+		p.sendMessage("§eDone");
 	}
 
 }
-
