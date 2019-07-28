70d47f22d2e7104cbabd7411f4f9511bf2f5038b
DianoxDragon <dianoxcontactpro@gmail.com>
2019-07-28 03:58:23 +0200

    Update TaskReloadServer.java

--- src/fr/Dianox/Hawn/Utility/Tasks/TaskReloadServer.java
+++ src/fr/Dianox/Hawn/Utility/Tasks/TaskReloadServer.java
@@ -4,9 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import fr.Dianox.Hawn.Utility.MessageUtils;
-import fr.Dianox.Hawn.Utility.Config.Messages.Adminstration.AdminPanelConfig;
-
 public class TaskReloadServer extends BukkitRunnable {
 	
 	private Player p;
@@ -19,9 +16,7 @@ public class TaskReloadServer extends BukkitRunnable {
 	public void run() {
 		Bukkit.getServer().reload();
 		
-		for (String msg : AdminPanelConfig.getConfig().getStringList("Special.Item.Reload.Messages")) {
-			MessageUtils.ReplaceCharMessagePlayer(msg, p);
-		}
+		p.sendMessage("§eDone");
 	}
 
 }
