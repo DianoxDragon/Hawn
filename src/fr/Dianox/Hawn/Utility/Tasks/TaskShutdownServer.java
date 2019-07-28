5cbb7cf9d767f899dd90786c25f7ce6bad3ce977
DianoxDragon <dianoxcontactpro@gmail.com>
2019-07-28 03:55:09 +0200

    Update TaskShutdownServer.java

--- src/fr/Dianox/Hawn/Utility/Tasks/TaskShutdownServer.java
+++ src/fr/Dianox/Hawn/Utility/Tasks/TaskShutdownServer.java
@@ -1,13 +1,27 @@
 package fr.Dianox.Hawn.Utility.Tasks;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class TaskShutdownServer extends BukkitRunnable {
+import fr.Dianox.Hawn.Utility.MessageUtils;
+import fr.Dianox.Hawn.Utility.Config.Messages.Adminstration.AdminPanelConfig;
 
+public class TaskShutdownServer extends BukkitRunnable {
+	
+	private Player p;
+
+	public TaskShutdownServer(Player p) {
+        this.p = p;
+	}
+	
 	@Override
 	public void run() {
 		Bukkit.getServer().shutdown();
+		
+		for (String msg : AdminPanelConfig.getConfig().getStringList("Special.Item.Shutdown.Messages")) {
+			MessageUtils.ReplaceCharMessagePlayer(msg, p);
+		}
 	}
 
 }
