7898c2d12d3ff2a04d080742df9561dcb85a7616
DianoxDragon <dianoxcontactpro@gmail.com>
2019-08-03 22:56:27 +0200

    Update TitleAnnouncerConfig.java

--- src/fr/Dianox/Hawn/Utility/Config/Commands/TitleAnnouncerConfig.java
+++ src/fr/Dianox/Hawn/Utility/Config/Commands/TitleAnnouncerConfig.java
@@ -53,17 +53,40 @@ public class TitleAnnouncerConfig {
                 file.createNewFile();
             } catch (IOException e) {}
 
-            Config.set("Title-Announcer.Enable", Boolean.valueOf(true));
-            Config.set("Title-Announcer.Title.FadeIn", Integer.valueOf(20));
-            Config.set("Title-Announcer.Title.Stay", Integer.valueOf(75));
-            Config.set("Title-Announcer.Title.FadeOut", Integer.valueOf(20));
-            Config.set("Title.Disable-Message", Boolean.valueOf(true));
-            Config.set("DISABLE_THE_COMMAND_COMPLETELY", Boolean.valueOf(false));
+            Config.set("Title-Announcer.Enable", true);
+            Config.set("Title-Announcer.Title.FadeIn", 20);
+            Config.set("Title-Announcer.Title.Stay", 75);
+            Config.set("Title-Announcer.Title.FadeOut", 20);
+            Config.set("Title-Announcer.Disable-Message", true);
+            
+            Config.set("Title-Announcer.Options.Write-In-The-Chat-The-Announce", true);
+            
+            Config.set("Title-Announcer.Options.Firework.Enable", true);
+            Config.set("Title-Announcer.Options.Firework.Amount", 2);
+            Config.set("Title-Announcer.Options.Firework.Height", 3);
+            Config.set("Title-Announcer.Options.Firework.Flicker", false);
+            Config.set("Title-Announcer.Options.Firework.Trail", false);
+            Config.set("Title-Announcer.Options.Firework.Type", "BALL");
+            Config.set("Title-Announcer.Options.Firework.Instant-explode", false);
+            Config.set("Title-Announcer.Options.Firework.Power", 3);
+            Config.set("Title-Announcer.Options.Firework.Colors", java.util.Arrays.asList(new String[] {
+                    "YELLOW",
+                    "RED"
+                }));
+            Config.set("Title-Announcer.Options.Firework.Fade", java.util.Arrays.asList(new String[] {
+                    "BLUE",
+                    "WHITE"
+                }));
+            
+            Config.set("Title-Announcer.Options.Sound-For-All-Players.Enable", true);
+            Config.set("Title-Announcer.Options.Sound-For-All-Players.Sound", "BLOCK_NOTE_HARP");
+            Config.set("Title-Announcer.Options.Sound-For-All-Players.Volume", 1);
+            Config.set("Title-Announcer.Options.Sound-For-All-Players.Pitch", 1);
+            
+            Config.set("DISABLE_THE_COMMAND_COMPLETELY", false);
 
             saveConfigFile();
 
         }
     }
-
-
 }
