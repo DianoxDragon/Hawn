e77211f98ff79913a0952f284d6c9115fc8f93f1
DianoxDragon <dianoxcontactpro@gmail.com>
2019-08-05 17:00:09 +0200

    Update OnCommandEvent.java

--- src/fr/Dianox/Hawn/Event/OnCommandEvent.java
+++ src/fr/Dianox/Hawn/Event/OnCommandEvent.java
@@ -81,23 +81,23 @@ public class OnCommandEvent implements Listener {
                                     String Permission = String.valueOf(CustomCommandConfig.getConfig().getString("commands." + string + ".permission.message"));
                                     MessageUtils.MessageNoPermission(p, Permission);
                                 }
-                                
+
                                 e.setCancelled(true);
                                 return;
                             }
                         }
-                        
+
                         for (String msg: CustomCommandConfig.getConfig().getStringList("commands." + string + ".message")) {
 
                             String perm = "";
 
                             if (msg.startsWith("<perm>") && msg.contains("</perm>")) {
-                            	perm = StringUtils.substringBetween(msg, "<perm>", "</perm>");
-                            	msg = msg.replace("<perm>"+perm+"</perm> ", "");
-                            	
-                            	if (!p.hasPermission(perm)) {
-                            		continue;
-                            	}
+                                perm = StringUtils.substringBetween(msg, "<perm>", "</perm>");
+                                msg = msg.replace("<perm>" + perm + "</perm> ", "");
+
+                                if (!p.hasPermission(perm)) {
+                                    continue;
+                                }
                             }
 
                             if (msg.startsWith("[command-player]: ")) {
@@ -105,6 +105,11 @@ public class OnCommandEvent implements Listener {
                                 msg = msg.replaceAll("%player%", p.getName());
 
                                 p.performCommand(msg);
+                            } else if (msg.startsWith("[customcommand-player]: ")) {
+                            	msg = msg.replace("[customcommand-player]: ", "");
+                                msg = msg.replaceAll("%player%", p.getName());
+                                
+                                executecustomcommand(msg, p);
                             } else if (msg.startsWith("[command-console]: ")) {
                                 msg = msg.replace("[command-console]: ", "");
                                 msg = msg.replaceAll("%player%", p.getName());
@@ -218,6 +223,153 @@ public class OnCommandEvent implements Listener {
                         }
                         e.setCancelled(true);
                     }
+                }
+            }
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    public static void executecustomcommand(String cmd, Player p) {
+        if (CustomCommandConfig.getConfig().getBoolean("commands." + cmd + ".enable")) {
+
+            if (CustomCommandConfig.getConfig().getBoolean("commands." + cmd + ".permission.enable")) {
+                if (!p.hasPermission(CustomCommandConfig.getConfig().getString("commands." + cmd + ".permission.message"))) {
+                    if (CustomCommandConfig.getConfig().getBoolean("commands." + cmd + ".no-permission-message-enable")) {
+                        String Permission = String.valueOf(CustomCommandConfig.getConfig().getString("commands." + cmd + ".permission.message"));
+                        MessageUtils.MessageNoPermission(p, Permission);
+                    }
+
+                    return;
+                }
+            }
+
+            for (String msg: CustomCommandConfig.getConfig().getStringList("commands." + cmd + ".message")) {
+
+                String perm = "";
+
+                if (msg.startsWith("<perm>") && msg.contains("</perm>")) {
+                    perm = StringUtils.substringBetween(msg, "<perm>", "</perm>");
+                    msg = msg.replace("<perm>" + perm + "</perm> ", "");
+
+                    if (!p.hasPermission(perm)) {
+                        continue;
+                    }
+                }
+
+                if (msg.startsWith("[command-player]: ")) {
+                    msg = msg.replace("[command-player]: ", "");
+                    msg = msg.replaceAll("%player%", p.getName());
+
+                    p.performCommand(msg);
+                } else if (msg.startsWith("[command-console]: ")) {
+                    msg = msg.replace("[command-console]: ", "");
+                    msg = msg.replaceAll("%player%", p.getName());
+
+                    Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), msg);
+                } else if (msg.startsWith("[gamemode-survival]")) {
+                    p.setGameMode(GameMode.SURVIVAL);
+                } else if (msg.startsWith("[gamemode-creative]")) {
+                    p.setGameMode(GameMode.CREATIVE);
+                } else if (msg.startsWith("[gamemode-adventure]")) {
+                    p.setGameMode(GameMode.ADVENTURE);
+                } else if (msg.startsWith("[gamemode-spectator]")) {
+                    p.setGameMode(GameMode.SPECTATOR);
+                } else if (msg.startsWith("[ping]")) {
+                    for (String msg1: ConfigMCommands.getConfig().getStringList("Ping.Self")) {
+                        MessageUtils.ReplaceCharMessagePlayer(msg1, p);
+                    }
+                } else if (msg.startsWith("[spawn]: ")) {
+                    msg = msg.replace("[spawn]: ", "");
+                    SpawnUtils.teleportToSpawn(p, msg);
+                } else if (msg.startsWith("[warp]: ")) {
+                    msg = msg.replace("[warp]: ", "");
+                    WarpCommand.onTp(p, msg);
+                } else if (msg.startsWith("[bungee]: ")) {
+                    msg = msg.replace("[bungee]: ", "");
+                    Bungee.changeServer(p, msg);
+                } else if (msg.startsWith("[effect[")) {
+                    msg = msg.replace("[effect[", "");
+
+                    String[] parts = msg.split("]]: ");
+
+                    p.addPotionEffect(new PotionEffect(PotionEffectType.getByName(parts[1]), 1999999999, Integer.valueOf(parts[0])));
+                } else if (msg.startsWith("[effectclear]: ")) {
+                    msg = msg.replace("[effectclear]: ", "");
+
+                    p.removePotionEffect(PotionEffectType.getByName(msg));
+                } else if (msg.startsWith("[effectclearall]")) {
+                    for (PotionEffect effect: p.getActivePotionEffects()) {
+                        p.removePotionEffect(effect.getType());
+                    }
+                } else if (msg.startsWith("[send-title]: ")) {
+                    msg = msg.replace("[send-title]: ", "");
+                    msg = msg.replaceAll("&", "§");
+
+                    Boolean activate = false;
+
+                    String title = "";
+                    String subtitle = "";
+
+                    if (msg.contains("//n")) {
+                        String[] parts = msg.split("//n");
+                        title = parts[0];
+                        subtitle = parts[1];
+
+                        title = title.replaceAll("//n", "");
+                        subtitle = subtitle.replaceAll("//n", "");
+
+                        activate = true;
+                    }
+
+                    if (activate == false) {
+                        TitleUtils.sendTitle(p, 20, 150, 75, msg);
+                    } else {
+                        TitleUtils.sendTitle(p, 20, 150, 75, title);
+                        TitleUtils.sendSubtitle(p, 20, 150, 75, subtitle);
+                    }
+                } else if (msg.startsWith("[send-title[")) {
+                    msg = msg.replace("[send-title[", "");
+
+                    String[] parts = msg.split("]]: ");
+
+                    Boolean activate = false;
+
+                    String title = "";
+                    String subtitle = "";
+
+                    if (msg.contains("//n")) {
+                        String[] part = parts[1].split("//n");
+                        title = part[0];
+                        subtitle = part[1];
+
+                        title = title.replaceAll("//n", "");
+                        subtitle = subtitle.replaceAll("//n", "");
+
+                        activate = true;
+                    }
+
+                    if (activate == false) {
+                        TitleUtils.sendTitle(p, 20, Integer.valueOf(parts[0]), 75, parts[1]);
+                    } else {
+                        TitleUtils.sendTitle(p, 20, Integer.valueOf(parts[0]), 75, title);
+                        TitleUtils.sendSubtitle(p, 20, Integer.valueOf(parts[0]), 75, subtitle);
+                    }
+                } else if (msg.startsWith("[send-actionbar]: ")) {
+                    msg = msg.replace("[send-actionbar]: ", "");
+                    msg = msg.replaceAll("&", "§");
+
+                    ActionBar.sendActionBar(p, msg);
+                } else if (msg.startsWith("[send-actionbar[")) {
+                    msg = msg.replace("[send-actionbar[", "");
+
+                    String[] parts = msg.split("]]: ");
+
+                    ActionBar.sendActionBar(p, parts[1], Integer.valueOf(parts[0]));
+                } else if (msg.startsWith("[sounds]: ")) {
+                    msg = msg.replace("[sounds]: ", "");
+                    p.playSound(p.getLocation(), XSound.matchXSound(msg).parseSound(), 1, 1);
+                } else {
+                    MessageUtils.ReplaceCharMessagePlayer(msg, p);
                 }
             }
         }
