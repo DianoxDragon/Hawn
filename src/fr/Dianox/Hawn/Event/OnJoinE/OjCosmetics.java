59e9d9e15e7b2547beff33eab0f9a60e95de7712
DianoxDragon <dianoxcontactpro@gmail.com>
2019-08-10 23:33:05 +0200

    Update OjCosmetics.java

--- src/fr/Dianox/Hawn/Event/OnJoinE/OjCosmetics.java
+++ src/fr/Dianox/Hawn/Event/OnJoinE/OjCosmetics.java
@@ -1,17 +1,8 @@
 package fr.Dianox.Hawn.Event.OnJoinE;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.bukkit.Bukkit;
-import org.bukkit.Color;
-import org.bukkit.FireworkEffect;
 import org.bukkit.World;
-import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.meta.FireworkMeta;
 
-import fr.Dianox.Hawn.Main;
 import fr.Dianox.Hawn.Utility.OtherUtils;
 import fr.Dianox.Hawn.Utility.Config.CosmeticsFun.ConfigGCos;
 import fr.Dianox.Hawn.Utility.World.CosmeticsPW;
@@ -29,19 +20,47 @@ public class OjCosmetics {
                         if (!p.hasPermission("hawn.event.onjoin.bypass.firework")) {
                         	if (ConfigGCos.getConfig().getBoolean("Cosmetics.Firework.Options.First-Join-Only")) {
                         		if (!p.hasPlayedBefore()) {
-                        			Fireworkmethod(p);
+                        			for (String s: ConfigGCos.getConfig().getStringList("Cosmetics.Firework.Options.Firework-List")) {
+                        				if (s.startsWith("[FWLU]: ")) {
+                        					
+                        					s = s.replace("[FWLU]: ", "");
+                        					
+                        					OtherUtils.Fireworkmethod(p, s);
+                        				}
+                        			}
                         		}
                         	} else {
-                        		Fireworkmethod(p);
+                        		for (String s: ConfigGCos.getConfig().getStringList("Cosmetics.Firework.Options.Firework-List")) {
+                    				if (s.startsWith("[FWLU]: ")) {
+                    					
+                    					s = s.replace("[FWLU]: ", "");
+                    					
+                    					OtherUtils.Fireworkmethod(p, s);
+                    				}
+                    			}
                         	}
                         }
                     } else {
                     	if (ConfigGCos.getConfig().getBoolean("Cosmetics.Firework.Options.First-Join-Only")) {
                     		if (!p.hasPlayedBefore()) {
-                    			Fireworkmethod(p);
+                    			for (String s: ConfigGCos.getConfig().getStringList("Cosmetics.Firework.Options.Firework-List")) {
+                    				if (s.startsWith("[FWLU]: ")) {
+                    					
+                    					s = s.replace("[FWLU]: ", "");
+                    					
+                    					OtherUtils.Fireworkmethod(p, s);
+                    				}
+                    			}
                     		}
                     	} else {
-                    		Fireworkmethod(p);
+                    		for (String s: ConfigGCos.getConfig().getStringList("Cosmetics.Firework.Options.Firework-List")) {
+                				if (s.startsWith("[FWLU]: ")) {
+                					
+                					s = s.replace("[FWLU]: ", "");
+                					
+                					OtherUtils.Fireworkmethod(p, s);
+                				}
+                			}
                     	}
                     }
                 }
@@ -50,19 +69,47 @@ public class OjCosmetics {
                     if (!p.hasPermission("hawn.event.onjoin.bypass.firework")) {
                     	if (ConfigGCos.getConfig().getBoolean("Cosmetics.Firework.Options.First-Join-Only")) {
                     		if (!p.hasPlayedBefore()) {
-                    			Fireworkmethod(p);
+                    			for (String s: ConfigGCos.getConfig().getStringList("Cosmetics.Firework.Options.Firework-List")) {
+                    				if (s.startsWith("[FWLU]: ")) {
+                    					
+                    					s = s.replace("[FWLU]: ", "");
+                    					
+                    					OtherUtils.Fireworkmethod(p, s);
+                    				}
+                    			}
                     		}
                     	} else {
-                    		Fireworkmethod(p);
+                    		for (String s: ConfigGCos.getConfig().getStringList("Cosmetics.Firework.Options.Firework-List")) {
+                				if (s.startsWith("[FWLU]: ")) {
+                					
+                					s = s.replace("[FWLU]: ", "");
+                					
+                					OtherUtils.Fireworkmethod(p, s);
+                				}
+                			}
                     	}
                     }
                 } else {
                 	if (ConfigGCos.getConfig().getBoolean("Cosmetics.Firework.Options.First-Join-Only")) {
                 		if (!p.hasPlayedBefore()) {
-                			Fireworkmethod(p);
+                			for (String s: ConfigGCos.getConfig().getStringList("Cosmetics.Firework.Options.Firework-List")) {
+                				if (s.startsWith("[FWLU]: ")) {
+                					
+                					s = s.replace("[FWLU]: ", "");
+                					
+                					OtherUtils.Fireworkmethod(p, s);
+                				}
+                			}
                 		}
                 	} else {
-                		Fireworkmethod(p);
+                		for (String s: ConfigGCos.getConfig().getStringList("Cosmetics.Firework.Options.Firework-List")) {
+            				if (s.startsWith("[FWLU]: ")) {
+            					
+            					s = s.replace("[FWLU]: ", "");
+            					
+            					OtherUtils.Fireworkmethod(p, s);
+            				}
+            			}
                 	}
                 }
             }
@@ -149,39 +196,5 @@ public class OjCosmetics {
             }
 		}
 	}
-	
-	public static void Fireworkmethod(Player p) {
-        for (int i = 1; i < ConfigGCos.getConfig().getInt("Cosmetics.Firework.Options.Amount"); i++) {
-            ArrayList < Color > colors = new ArrayList < Color > ();
-            ArrayList < Color > fade = new ArrayList < Color > ();
-            List < String > lore = ConfigGCos.getConfig().getStringList("Cosmetics.Firework.Options.Colors");
-            List < String > lore2 = ConfigGCos.getConfig().getStringList("Cosmetics.Firework.Options.Fade");
-            for (String l1: lore) {
-                colors.add(OtherUtils.getColor(l1));
-            }
-            for (String l2: lore2) {
-                fade.add(OtherUtils.getColor(l2));
-            }
-            final Firework f = p.getWorld().spawn(p.getLocation().add(0.5D, ConfigGCos.getConfig().getInt("Cosmetics.Firework.Options.Height"), 0.5D), Firework.class);
-
-            FireworkMeta fm = f.getFireworkMeta();
-            fm.addEffect(FireworkEffect.builder().flicker(ConfigGCos.getConfig().getBoolean("Cosmetics.Firework.Options.Flicker"))
-                .trail(ConfigGCos.getConfig().getBoolean("Cosmetics.Firework.Options.Trail"))
-                .with(FireworkEffect.Type.valueOf(ConfigGCos.getConfig().getString("Cosmetics.Firework.Options.Type"))).withColor(colors).withFade(fade)
-                .build());
-
-            if (!ConfigGCos.getConfig().getBoolean("Cosmetics.Firework.Options.Instant-explode")) {
-                fm.setPower(ConfigGCos.getConfig().getInt("Cosmetics.Firework.Options.Power"));
-            }
-            f.setFireworkMeta(fm);
-            if (ConfigGCos.getConfig().getBoolean("Cosmetics.Firework.Options.Instant-explode")) {
-                Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), new Runnable() {
-                    public void run() {
-                        f.detonate();
-                    }
-                }, 5L);
-            }
-        }
-    }
 
 }
