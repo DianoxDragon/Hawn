4d08d7684f5094ef18f90196b08c06188f399628
DianoxDragon <dianoxcontactpro@gmail.com>
2019-08-11 16:52:32 +0200

    Update ProtectionPlayerConfig.java

--- src/fr/Dianox/Hawn/Utility/Config/Events/ProtectionPlayerConfig.java
+++ src/fr/Dianox/Hawn/Utility/Config/Events/ProtectionPlayerConfig.java
@@ -53,9 +53,9 @@ public class ProtectionPlayerConfig {
                 file.createNewFile();
             } catch (IOException e) {}
             
-            Config.set("Anti-Damage.Enable", Boolean.valueOf(true));
-            Config.set("Anti-Damage.Custom.Enable", Boolean.valueOf(false));
-            Config.set("Anti-Damage.Bypass-With-Permission", Boolean.valueOf(false));
+            Config.set("Anti-Damage.Enable", true);
+            Config.set("Anti-Damage.Custom.Enable", false);
+            Config.set("Anti-Damage.Bypass-With-Permission", false);
             
             Config.set("Anti-Damage.WorldGuard.Enable", false);
             Config.set("Anti-Damage.WorldGuard.Method", "WHITELIST");
@@ -64,40 +64,40 @@ public class ProtectionPlayerConfig {
                     "whatyouwant"
                 }));
             
-            Config.set("Anti-Damage.World.All_World", Boolean.valueOf(false));
+            Config.set("Anti-Damage.World.All_World", false);
             Config.set("Anti-Damage.World.Worlds", java.util.Arrays.asList(new String[] {
                     "world",
                     "world_nether"
             }));
-            Config.set("AntiDamage-Custom.Entity.Options.Entity.EntityDamageByEntity", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.BLOCK_EXPLOSION", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.CONTACT", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.CRAMMING", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.CUSTOM", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.DROWNING", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.DRAGON_BREATH", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.DRYOUT", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.ENTITY_ATTACK", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.ENTITY_EXPLOSION", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.ENTITY_SWEEP_ATTACK", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.FALL", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.FALLING_BLOCK", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.FIRE", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.FIRE_TICK", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.FLY_INTO_WALL", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.HOT_FLOOR", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.LAVA", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.LIGHTNING", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.MAGIC", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.MELTING", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.POISON", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.PROJECTILE", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.STARVATION", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.SUFFOCATION", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.SUICIDE", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.THORNS", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.VOID", Boolean.valueOf(true));
-            Config.set("AntiDamage-Custom.Entity.Options.WITHER", Boolean.valueOf(true));
+            Config.set("AntiDamage-Custom.Entity.Options.Entity.EntityDamageByEntity", true);
+            Config.set("AntiDamage-Custom.Entity.Options.BLOCK_EXPLOSION", true);
+            Config.set("AntiDamage-Custom.Entity.Options.CONTACT", true);
+            Config.set("AntiDamage-Custom.Entity.Options.CRAMMING", true);
+            Config.set("AntiDamage-Custom.Entity.Options.CUSTOM", true);
+            Config.set("AntiDamage-Custom.Entity.Options.DROWNING", true);
+            Config.set("AntiDamage-Custom.Entity.Options.DRAGON_BREATH", true);
+            Config.set("AntiDamage-Custom.Entity.Options.DRYOUT", true);
+            Config.set("AntiDamage-Custom.Entity.Options.ENTITY_ATTACK", true);
+            Config.set("AntiDamage-Custom.Entity.Options.ENTITY_EXPLOSION", true);
+            Config.set("AntiDamage-Custom.Entity.Options.ENTITY_SWEEP_ATTACK", true);
+            Config.set("AntiDamage-Custom.Entity.Options.FALL", true);
+            Config.set("AntiDamage-Custom.Entity.Options.FALLING_BLOCK", true);
+            Config.set("AntiDamage-Custom.Entity.Options.FIRE", true);
+            Config.set("AntiDamage-Custom.Entity.Options.FIRE_TICK", true);
+            Config.set("AntiDamage-Custom.Entity.Options.FLY_INTO_WALL", true);
+            Config.set("AntiDamage-Custom.Entity.Options.HOT_FLOOR", true);
+            Config.set("AntiDamage-Custom.Entity.Options.LAVA", true);
+            Config.set("AntiDamage-Custom.Entity.Options.LIGHTNING", true);
+            Config.set("AntiDamage-Custom.Entity.Options.MAGIC", true);
+            Config.set("AntiDamage-Custom.Entity.Options.MELTING", true);
+            Config.set("AntiDamage-Custom.Entity.Options.POISON", true);
+            Config.set("AntiDamage-Custom.Entity.Options.PROJECTILE", true);
+            Config.set("AntiDamage-Custom.Entity.Options.STARVATION", true);
+            Config.set("AntiDamage-Custom.Entity.Options.SUFFOCATION", true);
+            Config.set("AntiDamage-Custom.Entity.Options.SUICIDE", true);
+            Config.set("AntiDamage-Custom.Entity.Options.THORNS", true);
+            Config.set("AntiDamage-Custom.Entity.Options.VOID", true);
+            Config.set("AntiDamage-Custom.Entity.Options.WITHER", true);
             
             saveConfigFile();
 
