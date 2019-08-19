f06e50c4a150a7d788d8f35ad0cc8a7fc6957be5
DianoxDragon <dianoxcontactpro@gmail.com>
2019-08-19 16:09:40 +0200

    Update ChangeWorldPW.java

--- src/fr/Dianox/Hawn/Utility/World/ChangeWorldPW.java
+++ src/fr/Dianox/Hawn/Utility/World/ChangeWorldPW.java
@@ -12,6 +12,7 @@ public class ChangeWorldPW {
     public static List < String > world_change_keepfly = new ArrayList < String > ();
     public static List < String > worlds_GM_OnChangeWorld = new ArrayList < String > ();
     public static List < String > worlds_po = new ArrayList < String > ();
+    public static List < String > worlds_commands = new ArrayList < String > ();
 
     // Fly
     public static void setWKEEPFLY() {
@@ -51,7 +52,24 @@ public class ChangeWorldPW {
             }
         }
     }
+    
+    // Commands
+    public static void setCommands() {
+        if (PlayerWorldChangeConfigE.getConfig().getBoolean("Execute-Command.Enable") && !PlayerWorldChangeConfigE.getConfig().getBoolean("Execute-Command.World.All_World")) {
+            for (String world: PlayerWorldChangeConfigE.getConfig().getStringList("Execute-Command.World.Worlds")) {
+                if (Bukkit.getWorld(world) == null) {
+                    System.out.println("| Invalid world in Events/PlayerWorldChange.yml, Execute-Command.World: " + world);
+                } else {
+                	worlds_commands.add(world);
+                }
+            }
+        }
+    }
 
+    public static List < String > getCommands() {
+        return worlds_commands;
+    }
+    
     public static List < String > getWPO() {
         return worlds_po;
     }
