990ccbf4a8d3d453b26cae5d01ada8a966a9dca1
DianoxDragon <dianoxcontactpro@gmail.com>
2019-08-24 12:03:47 +0200

    Update ConfigMOStuff.java

--- src/fr/Dianox/Hawn/Utility/Config/Messages/ConfigMOStuff.java
+++ src/fr/Dianox/Hawn/Utility/Config/Messages/ConfigMOStuff.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
+import fr.Dianox.Hawn.Main;
+
 public class ConfigMOStuff {
 	
 	private static Plugin pl;
@@ -17,7 +19,7 @@ public class ConfigMOStuff {
 	public static void loadConfig(Plugin plugin) {
 		pl = plugin;
 		
-		file = new File(pl.getDataFolder(), "Messages/Classic/SomeOtherStuff.yml");
+		file = new File(pl.getDataFolder(), "Messages/" + Main.LanguageType + "/Classic/SomeOtherStuff.yml");
 		Config = YamlConfiguration.loadConfiguration(file);
 		
 		if (!pl.getDataFolder().exists()) {
