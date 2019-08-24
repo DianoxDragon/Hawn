722b54a86b43992ca4761cef3d3b3ba7ce15ba86
DianoxDragon <dianoxcontactpro@gmail.com>
2019-08-24 12:08:01 +0200

    Update and rename src/fr/Dianox/Hawn/Utility/Config/Messages/Adminstration/OtherAMConfig.java to src/fr/Dianox/Hawn/Utility/Config/Messages/Administration/OtherAMConfig.java

--- src/fr/Dianox/Hawn/Utility/Config/Messages/Administration/OtherAMConfig.java
+++ src/fr/Dianox/Hawn/Utility/Config/Messages/Administration/OtherAMConfig.java
@@ -1,10 +1,12 @@
-package fr.Dianox.Hawn.Utility.Config.Messages.Adminstration;
+package fr.Dianox.Hawn.Utility.Config.Messages.Administration;
 
 import java.io.File;
 import java.io.IOException;
 
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
+
+import fr.Dianox.Hawn.Main;
 
 public class OtherAMConfig {
 	
@@ -17,7 +19,7 @@ public class OtherAMConfig {
 	public static void loadConfig(Plugin plugin) {
 		pl = plugin;
 		
-		file = new File(pl.getDataFolder(), "Messages/Administration/Others.yml");
+		file = new File(pl.getDataFolder(), "Messages/" + Main.LanguageType + "/Administration/Others.yml");
 		Config = YamlConfiguration.loadConfiguration(file);
 		
 		if (!pl.getDataFolder().exists()) {
